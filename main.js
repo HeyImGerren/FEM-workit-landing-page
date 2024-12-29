87da472f4bafac6f30982a19cf9f7891d2010d5c
Gerren Penaloza <gerren.penaloza@gmail.com>
2024-12-28 16:47:03 -0800

    CTA section tablet view done

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("hello world");
+
+document.querySelectorAll("*").forEach((el) => {
+  if (el.scrollWidth > el.clientWidth) {
+    console.log("Found the w0rst element ever: ", el);
+  }
+});
+
+console.log("test");
